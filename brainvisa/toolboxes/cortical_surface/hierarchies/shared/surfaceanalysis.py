ffc507a6e193d86662ac3fab6db66bafb688de91
coulon <coulon@bioproj.invalid>
2007-12-13 14:49:21 +0000

    forgot this file
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perforce/cortical_surface/main@25523 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/hierarchies/shared/surfaceanalysis.py
+++ brainvisa/toolboxes/cortical_surface/hierarchies/shared/surfaceanalysis.py
@@ -10,12 +10,6 @@ insert( 'nomenclature','surfaceanalysis',
   )
 )
 
-insertFirst('nomenclature/translation',
-  'surfaceReferential', SetType( "Surface Label Translation" ),
-  'surfaceRefModel_par', SetType( "Latitude Constraint Gyri Model" ),
-  'surfaceRefModel_mer', SetType( "Longitude Constraint Gyri Model" ),
-)
-
 
 insert( 'hemitemplate',
   "*PoleLeft", SetType( "Left Cingular Pole Template" ), 
