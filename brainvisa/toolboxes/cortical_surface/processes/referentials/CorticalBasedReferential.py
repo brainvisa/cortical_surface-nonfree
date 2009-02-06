ddd23d214e511438c09ce4203194e20e185514dc
coulon <coulon@bioproj.invalid>
2009-02-06 11:43:14 +0000

    added the sulcus selection field ('label' or 'name') in the parent process signature, otherwise nobody remembers to change it if needed.
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/perforce/cortical_surface/main@31497 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- brainvisa/toolboxes/cortical_surface/processes/referentials/CorticalBasedReferential.py
+++ brainvisa/toolboxes/cortical_surface/processes/referentials/CorticalBasedReferential.py
@@ -8,13 +8,15 @@ userLevel = 2
 
 signature = Signature( 
   'Lgraph', ReadDiskItem( 'Cortical folds graph', 'Graph',requiredAttributes={ 'side': 'left' } ),
-  'Rgraph', ReadDiskItem( 'Cortical folds graph', 'Graph',requiredAttributes={ 'side': 'right' } )
+  'Rgraph', ReadDiskItem( 'Cortical folds graph', 'Graph',requiredAttributes={ 'side': 'right' } ),
+  'sulcus_identification',Choice('name','label')
   )
 
 
 def initialization( self ):
     self.linkParameters( 'Lgraph','Rgraph')
     self.linkParameters( 'Rgraph','Lgraph')
+    self.sulcus_identification='label'
     eNode = SerialExecutionNode( self.name, parameterized=self )
 
     eNode.addChild( 'LeftHemisphere_Process',
@@ -25,5 +27,8 @@ def initialization( self ):
 
     eNode.addLink( 'LeftHemisphere_Process.Lgraph', 'Lgraph' )
     eNode.addLink( 'RightHemisphere_Process.Rgraph', 'Rgraph' )
+
+    eNode.addLink( 'LeftHemisphere_Process.ConstraintProjectionLeft.sulcus_identification', 'sulcus_identification')
+    eNode.addLink( 'RightHemisphere_Process.ConstraintProjectionRight.sulcus_identification', 'sulcus_identification')
     
     self.setExecutionNode( eNode )
