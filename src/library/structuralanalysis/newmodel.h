2ccc4d0d3a8f7c6a18f27b517f15a8c1734c7ec6
operto <operto@bioproj.invalid>
2010-09-16 16:06:57 +0000

    working on a possible new model
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-private/trunk@39077 613412ce-1361-4b7a-93bf-a3c75af1bae4

---
+++ src/library/structuralanalysis/newmodel.h
@@ -0,0 +1,20 @@
+#ifndef AIMS_ANNEAL_H
+#define AIMS_ANNEAL_H
+#include <cortical_surface/structuralanalysis/minimization.h>
+#include <cortical_surface/structuralanalysis/cliques.h>
+
+
+class NewModel: public SurfaceBased_StructuralAnalysis{
+    private:
+
+        public:
+
+            void Run( int verbose = 1 );
+            void Step( std::vector<int> &random, long double temp, uint &mod );
+};
+
+
+
+
+#endif
+
