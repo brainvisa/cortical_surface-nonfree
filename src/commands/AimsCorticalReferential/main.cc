81bdc45b448accf45f7e8f3d2f33c518220dbeca
operto <operto@bioproj.invalid>
2011-02-17 18:05:51 +0000

    cleaned some code
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-private/trunk@41054 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/commands/AimsCorticalReferential/main.cc
+++ src/commands/AimsCorticalReferential/main.cc
@@ -11,75 +11,82 @@
 #include <cartobase/stream/fileutil.h>
 
 #include <cortical_surface/mesh/isoLine.h>
-
 #include <cortical_surface/surfacereferential/corticalReferential.h>
-
-using namespace aims;
-using namespace carto;
-using namespace std;
 
 
 int main(int argc, const char **argv)
 {
-  try
+    try
     {
-	std::string adress_mesh;
-	std::string adress_texMer;
-	std::string adress_texPar;
-	std::string adress_texCall;
-	std::string adress_texPoles;
-	std::string x;
-	std::string y;
-	float criter;
-	float dt;
-	float t;
-	int c;
-	int tBeta;
-	int choice;
-	AimsSurfaceTriangle mesh;
-	AimsApplication     app( argc, argv, "Diffusion de coordonnees sur une surface contrainte");
-	app.addOption( adress_mesh, "-i", "inputMesh");
-	app.alias( "--inMesh", "-i" );
-	app.addOption( adress_texPar, "-p", "input Texture Parallel Constraints");
-	app.alias( "--inTexPar", "-p" );
-	app.addOption( adress_texMer, "-m", "input Texture Meridian Constraints");
-	app.alias( "--inTexMer", "-m" );
-	app.addOption( adress_texCall, "-l", "input Texture Corpus Callosum Pole", "pole_calleux_plein.tex");
-	app.alias( "--inTexCall", "-l" );
-	app.addOption( adress_texPoles, "-r", "input Texture Poles");
-	app.alias( "--inTexPoles", "-r" );
-	app.addOption( criter, "-c", "Limit for the diffusion process ( default = 1e-6 )");
-	app.alias( "--criter", "-c" );
-	app.addOption( dt, "-d", "Iterative step ( default = 0.2 )");
-	app.alias( "--dt", "-d" );
-	app.addOption( c, "-b", "context");
- 	app.alias( "--bool", "-b" );
-	app.addOption( choice, "-f", "choice");
- 	app.alias( "--choice", "-f" );
-	app.addOption( t, "-t", "attache");
-	app.alias( "--attache", "-t" );
-	app.addOption( tBeta, "-a", "Beta Map (1=Yes, 0=No)");
-	app.alias( "--tBeta", "-a" );
-	app.addOption( x, "-x", "latitude texture output");
- 	app.alias( "--latitude", "-x" );
-	app.addOption( y, "-y", "longitude texture output");
- 	app.alias( "--longitude", "-y" );
-	app.initialize();
+    	std::string adress_mesh;
+    	std::string adress_texMer;
+    	std::string adress_texPar;
+    	std::string adress_texCall;
+    	std::string adress_texPoles;
+    	std::string x;
+    	std::string y;
+    	float criter;
+    	float dt;
+    	float t;
+    	int c;
+    	int tBeta;
+    	int choice;
+    	AimsSurfaceTriangle mesh;
+    	aims::AimsApplication     app( argc, argv, "Diffusion de coordonnees sur une surface contrainte");
+    	app.addOption( adress_mesh, "-i", "inputMesh");
+    	app.alias( "--inMesh", "-i" );
+    	app.addOption( adress_texPar, "-p", "input Texture Parallel Constraints");
+    	app.alias( "--inTexPar", "-p" );
+    	app.addOption( adress_texMer, "-m", "input Texture Meridian Constraints");
+    	app.alias( "--inTexMer", "-m" );
+    	app.addOption( adress_texCall, "-l", "input Texture Corpus Callosum Pole", "pole_calleux_plein.tex");
+    	app.alias( "--inTexCall", "-l" );
+    	app.addOption( adress_texPoles, "-r", "input Texture Poles");
+    	app.alias( "--inTexPoles", "-r" );
+    	app.addOption( criter, "-c", "Limit for the diffusion process ( default = 1e-6 )");
+    	app.alias( "--criter", "-c" );
+    	app.addOption( dt, "-d", "Iterative step ( default = 0.2 )");
+    	app.alias( "--dt", "-d" );
+    	app.addOption( c, "-b", "context");
+     	app.alias( "--bool", "-b" );
+    	app.addOption( choice, "-f", "choice");
+     	app.alias( "--choice", "-f" );
+    	app.addOption( t, "-t", "attache");
+    	app.alias( "--attache", "-t" );
+    	app.addOption( tBeta, "-a", "Beta Map (1=Yes, 0=No)");
+    	app.alias( "--tBeta", "-a" );
+    	app.addOption( x, "-x", "latitude texture output");
+     	app.alias( "--latitude", "-x" );
+    	app.addOption( y, "-y", "longitude texture output");
+     	app.alias( "--longitude", "-y" );
+    	app.initialize();
 
-	CorticalReferential cr(adress_mesh, adress_texPar, adress_texMer, adress_texCall, adress_texPoles, criter, dt,c,choice,t,tBeta,x,y);
-	cr.process();
-	return 0;
+    	aims::CorticalReferential cr ( adress_mesh, 
+    	                               adress_texPar, 
+    	                               adress_texMer, 
+    	                               adress_texCall, 
+    	                               adress_texPoles, 
+    	                               criter, 
+    	                               dt,
+    	                               c,
+    	                               choice,
+    	                               t,
+    	                               tBeta,
+    	                               x,
+    	                               y );
+    	cr.process();
+    	return 0;
 
     }
-   catch( user_interruption & )
+    catch( carto::user_interruption & )
     {
     }
-  catch( exception & e )
+    catch( std::exception & e )
     {
-      cerr << e.what() << endl;
-      return EXIT_FAILURE;
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
     }
 
-  return( EXIT_SUCCESS );
+    return( EXIT_SUCCESS );
 }
 
--- src/library/surfacereferential/corticalTools.cc
+++ src/library/surfacereferential/corticalTools.cc
@@ -13,361 +13,302 @@
 #include <cortical_surface/surfacereferential/corticalTools.h>
 #include <cortical_surface/surfacereferential/corticalConstraints.h>
 
-using namespace aims;
 using namespace std;
+/****************TOOLS****************/
 
+//initialization of single textures
+void aims::init_texture_single(TimeTexture<float> & texture)
+{
+	int size=texture[0].nItem();
+	for (int i=0;i<size;i++)
+		texture[0].item(i)=0;
+}
 
-	/****************TOOLS****************/
-
-	//initialization of single textures
-	void aims::init_texture_single(TimeTexture<float> & texture)
+TimeTexture<float> aims::dilate_texture(TimeTexture<float> & texture, float val, std::vector<std::set<uint> > neigh, AimsSurfaceTriangle mesh)
+{
+	//Val = coef multiplicateur pour la valeur des voisins (1 pour C, 0.5 pour Beta)
+	
+	int size=texture[0].nItem();
+	TimeTexture<float> result(1,size);
+	init_texture_single(result);
+	//std::cout<<"dilate1"<<std::endl;
+	for(int i=0; i<size; i++)
 	{
-		int size=texture[0].nItem();
-		for (int i=0;i<size;i++)
-			texture[0].item(i)=0;
-	}
-
-	TimeTexture<float> aims::dilate_texture(TimeTexture<float> & texture, float val, std::vector<std::set<uint> > neigh, AimsSurfaceTriangle mesh)
-	{
-		//Val = coef multiplicateur pour la valeur des voisins (1 pour C, 0.5 pour Beta)
-		
-		int size=texture[0].nItem();
-		TimeTexture<float> result(1,size);
-		init_texture_single(result);
-		//std::cout<<"dilate1"<<std::endl;
-		for(int i=0; i<size; i++)
+		if(texture[0].item(i)!=0)
 		{
-			if(texture[0].item(i)!=0)
+			//std::cout<<"test OK"<<std::endl;
+			result[0].item(i)=texture[0].item(i);
+			std::set<uint>::const_iterator itvoisin;
+			itvoisin=neigh[i].begin();
+			for(; itvoisin!=neigh[i].end(); itvoisin++)
 			{
-				//std::cout<<"test OK"<<std::endl;
-				result[0].item(i)=texture[0].item(i);
-				std::set<uint>::const_iterator itvoisin;
-				itvoisin=neigh[i].begin();
-				for(; itvoisin!=neigh[i].end(); itvoisin++)
-				{
-					//std::cout<<"vois"<<std::endl;
-					if(texture[0].item(*itvoisin)==0)
-					      result[0].item(*itvoisin)=texture[0].item(i)*val;
-				}
+				//std::cout<<"vois"<<std::endl;
+				if(texture[0].item(*itvoisin)==0)
+				      result[0].item(*itvoisin)=texture[0].item(i)*val;
 			}
 		}
-		return result;
 	}
-	
-	float aims::compute2D_distance(float lon1, float lon2, float lat1, float lat2)
+	return result;
+}
+
+float aims::compute2D_distance(float lon1, float lon2, float lat1, float lat2)
+{
+	float d, t1, t2, t;
+	t1=pow(lon1-lon2,2);
+	if(lon1>lon2)
+		t2=pow(lon2-lon1-360,2);
+	else
+		t2=pow(lon1-lon2-360,2);
+	if(t1<t2)	t=t1;
+	else		t=t2;
+
+	d=sqrt( t + pow( lat1-lat2,2) );
+
+	return d;
+}
+
+//Poles points determination (to be improved)
+std::pair<int,int> aims::find_poles ( TimeTexture<float> & tex, AimsSurfaceTriangle mesh )
+{
+	int size = tex[0].nItem();
+	std::cout << "SIZE = " << size << std::endl;
+	TimeTexture<float> distance_nord ( 1, size );
+	TimeTexture<float> distance_sud ( 1, size );
+	TimeTexture<float> pole1 ( 1, size );
+	TimeTexture<float> pole2 ( 1, size );
+	std::pair<int,int> poles_points;
+	//Textures used in meshdistance creation
+	for ( int i = 0 ; i < size ; i++ ) 
 	{
-		float d, t1, t2, t;
-		t1=pow(lon1-lon2,2);
-		if(lon1>lon2)
-			t2=pow(lon2-lon1-360,2);
+		if ( tex[0].item(i) == 1 ) 
+		{
+			pole1[0].item(i) = 0;
+			pole2[0].item(i) = 360;
+		}
 		else
-			t2=pow(lon1-lon2-360,2);
-		if(t1<t2)	t=t1;
-		else		t=t2;
-	
-		d=sqrt( t + pow( lat1-lat2,2) );
-	
-		return d;
+			if ( tex[0].item(i) == 180 ) 
+			{
+				pole1[0].item(i) = 360;
+				pole2[0].item(i) = 0;
+			}
+			else 
+			{
+				pole1[0].item(i) = 360;
+				pole2[0].item(i) = 360;
+			}
 	}
+
+	//std::cout<<"distance map for pole1 (corpus callosum)"<<std::endl;	
+	distance_nord[0] = meshdistance::MeshDistance( mesh[0], pole1[0],true);
+	//std::cout<<"distance map for pole1 (insula)"<<std::endl;	
+	distance_sud[0] = meshdistance::MeshDistance( mesh[0], pole2[0],true);
+
+ 	Writer<Texture1d> w("/home/go224932/test_pole_sud.tex");
+ 	w.write(distance_sud);
+ 	Writer<Texture1d> w1("/home/go224932/test_pole_nord.tex");
+ 	w1.write(distance_nord);
 	
-	//Poles points determination (to be improved)
-	std::pair<int,int> aims::find_poles(TimeTexture<float> & tex, AimsSurfaceTriangle mesh)
+	//CALCUL POLES NORD ET SUD DANS LE POLE CALLEUX ET LE POLE INSULAIRE
+
+	int max_nord = 0, max_sud = 0;
+	float max_value_nord = 0, max_value_sud = 0;
+
+	for ( int a = 0; a < size ; a++)
 	{
-		int size=tex[0].nItem();
-		std::cout<<"SIZE="<<size<<std::endl;
-		TimeTexture<float> distance_nord(1,size);
-		TimeTexture<float> distance_sud(1,size);
-		TimeTexture<float> pole1(1,size);
-		TimeTexture<float> pole2(1,size);
-		std::pair<int,int> poles_points;
-		//Textures used in meshdistance creation
-		for(int i=0;i<size; i++) 
+		if ( distance_nord[0].item(a) > max_value_nord )
 		{
-			if(tex[0].item(i)==1) 
-			{
-				pole1[0].item(i)=0;
-				pole2[0].item(i)=360;
-			}
-			else
-				if(tex[0].item(i)==180) 
-				{
-					pole1[0].item(i)=360;
-					pole2[0].item(i)=0;
-				}
-				else 
-				{
-					pole1[0].item(i)=360;
-					pole2[0].item(i)=360;
-				}
+			max_nord = a;
+			max_value_nord = distance_nord[0].item(a);
 		}
 
-		//std::cout<<"distance map for pole1 (corpus callosum)"<<std::endl;	
-		distance_nord[0]=meshdistance::MeshDistance( mesh[0], pole1[0],true);
-		//std::cout<<"distance map for pole1 (insula)"<<std::endl;	
-		distance_sud[0]=meshdistance::MeshDistance( mesh[0], pole2[0],true);
-
-// 	Writer<Texture1d> w("test_pole_sud.tex");
-// 	w.write(distance_sud);
-// 	Writer<Texture1d> w1("test_pole_nord.tex");
-// 	w1.write(distance_nord);
-	
-		//CALCUL POLES NORD ET SUD DANS LE POLE CALLEUX ET LE POLE INSULAIRE
-
-		int max_nord=0, max_sud=0;
-		float max_value_nord=0, max_value_sud=0;
-	
-		for(int a=0; a<size; a++)
+		if ( distance_sud[0].item(a) > max_value_sud )
 		{
- 			if(distance_nord[0].item(a)>max_value_nord)
- 			{
- 				max_nord=a;
- 				max_value_nord=distance_nord[0].item(a);
- 			}
-
-			if(distance_sud[0].item(a)>max_value_sud)
-			{
-				max_sud=a;
-				max_value_sud=distance_sud[0].item(a);
-			}
+			max_sud = a;
+			max_value_sud = distance_sud[0].item(a);
 		}
-		
-		std::cout<<"nord="<<max_nord<<" et sud ="<<max_sud<<std::endl;
-		
-		//TEST autre methode de determination du pole nord
-
-	
-		float Xmin,Zmin,Xmax,Zmax;
-		Xmin=10000;
-		Zmin=10000;
-		Xmax=0;
-		Zmax=0;
-		TimeTexture<float> poleN(1,size);
-		for(int i=0; i<size; i++)
-		{
-			float x=0;
-			float z=0;
-			if( distance_nord[0].item(i)!=0 )
-			{
-		//std::cout<<"lele1"<<endl;
-				x=mesh[0].vertex()[i][1];
-				z=mesh[0].vertex()[i][2];
-				if( z>Zmax)
-					Zmax=z;
-				if( z<Zmin)
-					Zmin=z;
-				if( x>Xmax)
-					Xmax=x;
-				if( x<Xmin)
-					Xmin=x;
-			}
-		}
-		
-		//std::cout<<"lele"<<endl;
-		float xT,zT,Delta;
-		xT=(Xmin+Xmax)/2;
-		zT=(Zmin+Zmax)/2;
-		Delta = 2;
-		for(int i=0; i<size; i++)
-		{
-			if( distance_nord[0].item(i)!=0 )
-			{
-				float xtemp, ztemp;
-				//std::cout<<"haha"<<endl;
-				xtemp=mesh[0].vertex()[i][1]-xT;
-				ztemp=mesh[0].vertex()[i][2]-zT;
-				if( ( sqrt(xtemp*xtemp) <= Delta ) && ( sqrt(ztemp*ztemp) <= Delta ) )
-					max_nord=i;
-			}
-		}
-		
-		//TEST autre methode de determination du pole sud
-
-	
-		//float Xmin,Zmin,Xmax,Zmax;
-		Xmin=10000;
-		Zmin=10000;
-		Xmax=0;
-		Zmax=0;
-		TimeTexture<float> poleS(1,size);
-		for(int i=0; i<size; i++)
-		{
-			float x=0;
-			float z=0;
-			if( distance_sud[0].item(i)!=0 )
-			{
-		//std::cout<<"lele1"<<endl;
-				x=mesh[0].vertex()[i][1];
-				z=mesh[0].vertex()[i][2];
-				if( z>Zmax)
-					Zmax=z;
-				if( z<Zmin)
-					Zmin=z;
-				if( x>Xmax)
-					Xmax=x;
-				if( x<Xmin)
-					Xmin=x;
-			}
-		}
-		
-		//std::cout<<"lele"<<endl;
-		//float xT,zT,Delta;
-		xT=(Xmin+Xmax)/2;
-		zT=(Zmin+Zmax)/2;
-		Delta = 2;
-		for(int i=0; i<size; i++)
-		{
-			if( distance_sud[0].item(i)!=0 )
-			{
-				float xtemp, ztemp;
-				//std::cout<<"haha"<<endl;
-				xtemp=mesh[0].vertex()[i][1]-xT;
-				ztemp=mesh[0].vertex()[i][2]-zT;
-				if( ( sqrt(xtemp*xtemp) <= Delta ) && ( sqrt(ztemp*ztemp) <= Delta ) )
-					max_sud=i;
-			}
-		}
-		
-		
-		//Afectation valeurs
-		std::cout<<"nord="<<max_nord<<" et sud ="<<max_sud<<std::endl;
-		poles_points.first=max_nord;
-		poles_points.second=max_sud;
-
-		return poles_points;
 	}
+	
+	std::cout << "nord=" << max_nord << " et sud =" << max_sud << std::endl;
+	
+	//TEST autre methode de determination du pole nord
+
+	float Xmin = 10000,
+	      Zmin = 10000,
+	      Xmax = 0,
+	      Zmax = 0;
+	TimeTexture<float> poleN ( 1, size );
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		float x = 0,
+		      z = 0;
+		if ( distance_nord[0].item(i) != 0 )
+		{
+			x = mesh[0].vertex()[i][1];
+			z = mesh[0].vertex()[i][2];
+			if ( z > Zmax )
+				Zmax = z;
+			if ( z < Zmin )
+				Zmin = z;
+			if ( x > Xmax )
+				Xmax = x;
+			if ( x < Xmin )
+				Xmin = x;
+		}
+	}
+	
+	float xT, zT, Delta;
+	xT = ( Xmin + Xmax ) / 2;
+	zT = ( Zmin + Zmax ) / 2;
+	Delta = 2;
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		if ( distance_nord[0].item(i) != 0 )
+		{
+			float xtemp, ztemp;
+			xtemp = mesh[0].vertex()[i][1] - xT;
+			ztemp = mesh[0].vertex()[i][2] - zT;
+			if ( ( sqrt(xtemp*xtemp) <= Delta ) && ( sqrt(ztemp*ztemp) <= Delta ) )
+				max_nord=i;
+		}
+	}
+	
+	//TEST autre methode de determination du pole sud
+	Xmin = 10000;
+	Zmin = 10000;
+	Xmax = 0;
+	Zmax = 0;
+	TimeTexture<float> poleS (1, size );
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		float x = 0,
+		      z = 0;
+		if ( distance_sud[0].item(i) != 0 )
+		{
+			x = mesh[0].vertex()[i][1];
+			z = mesh[0].vertex()[i][2];
+			if ( z > Zmax )
+				Zmax = z;
+			if ( z < Zmin )
+				Zmin = z;
+			if ( x > Xmax )
+				Xmax = x;
+			if ( x < Xmin )
+				Xmin = x;
+		}
+	}
+	
+	xT = ( Xmin + Xmax ) / 2;
+	zT = ( Zmin + Zmax ) / 2;
+	Delta = 2;
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		if ( distance_sud[0].item(i)!=0 )
+		{
+			float xtemp, ztemp;
+			xtemp = mesh[0].vertex()[i][1] - xT;
+			ztemp = mesh[0].vertex()[i][2] - zT;
+			if( ( sqrt(xtemp*xtemp) <= Delta ) && ( sqrt(ztemp*ztemp) <= Delta ) )
+				max_sud=i;
+		}
+	}
+	
+	//Afectation valeurs
+	std::cout<<"nord="<<max_nord<<" et sud ="<<max_sud<<std::endl;
+	poles_points.first = max_nord;
+	poles_points.second = max_sud;
+
+	return poles_points;
+}
         
-	void aims::drawGyri(std::string & adLong, std::string & adLat, std::string & adOut, std::string & adr_corl, std::string & side)
+void aims::drawGyri(std::string & adLong, std::string & adLat, std::string & adOut, std::string & adr_corl, std::string & side)
+{
+	
+	TimeTexture<float> longitude, latitude;
+	int size;
+	
+	Reader< TimeTexture<float> > longR ( adLong );
+	longR.read(longitude);
+	Reader< TimeTexture<float> > latR ( adLat );
+	latR.read(latitude);
+	
+	size = longitude.nItem();
+	
+	TimeTexture<float> gyri ( 1,size );
+	
+	std::map< std::string, std::set<float> > map_global;
+
+	const char *adr_cor= adr_corl.c_str();
+
+	FILE *corres; //correspondance between contraints names and real values
+	
+	if ((corres=fopen(adr_cor, "r")) == NULL)
 	{
+		cerr << "Cannot open file " << adr_cor << endl;
+		exit(EXIT_FAILURE);
+	}
+
+	std::string arg1;
 		
-		TimeTexture<float> longitude;
-		TimeTexture<float> latitude;
-		
-		int size;
-		
-		//cout<<"Adress Longitude = "<<adLong<<endl;
-		//cout<<"Adress Latitude = "<<adLat<<endl;
-		
-		Reader< TimeTexture<float> > longR( adLong );
-		longR.read(longitude);
-		Reader< TimeTexture<float> > latR( adLat );
-		latR.read(latitude);
-		
-		size=longitude.nItem();
-		
-		TimeTexture<float> gyri(1,size);
-		
-		std::map< std::string, std::set<float> > map_global;
+	int val_contraint;
+	int val_projection;
 	
-		const char *adr_cor= adr_corl.c_str();
-	
-		FILE *corres; //correspondance between contraints names and real values
+	while ( ! feof(corres) )
+	{
+		char c[40];
+		char a[5];
+		fscanf ( corres, "%s %s %i\n", a, c, &val_projection );
+		arg1 = a;
+		map_global[a].insert ( val_projection );
+	}
 		
-		if ((corres=fopen(adr_cor, "r")) == NULL)
+	float u = 0,
+	      v = 0;
+	
+	std::set<float>::const_iterator it_lon, it_lat, it_lon2, it_lat2;
+		
+	for ( int i = 0 ; i < size ; i++ )
+	{
+		gyri[0].item(i) = 0;
+		int cpt = 2;
+		
+		u = latitude[0].item(i);
+		v = longitude[0].item(i);
+		
+		it_lat = map_global["lat"].begin();
+		for ( ; it_lat != map_global["lat"].end() ; ++it_lat )
 		{
-			cerr << "Cannot open file " << adr_cor << endl;
-			exit(EXIT_FAILURE);
+			it_lat2 = it_lat;
+			it_lat2++;
+			if ( it_lat2 != map_global["lat"].end() )
+			{
+				it_lon = map_global["lon"].begin();
+				for ( ; it_lon != map_global["lon"].end() ; ++it_lon )
+				{
+					it_lon2 = it_lon;
+					it_lon2++;
+					if ( it_lon2 != map_global["lon"].end() )
+					{
+						if ( u > (*it_lat) && u <= (*it_lat2) && v > (*it_lon) && v <= (*it_lon2) )
+						{
+							gyri[0].item(i)=cpt;
+						}
+						cpt++;
+					}
+				}
+			}
 		}
-	
-		std::string arg1;
 		
-/*	char *  arg1;
-		char *  arg2;*/
-		int val_contraint;
-		int val_projection;
+		//Pole cingulaire
 		
-		while (!feof(corres))
-		{
-			char c[40];
-			char a[5];
-			fscanf(corres, "%s %s %i\n", a, c, &val_projection);
-			arg1=a;
-			map_global[a].insert(val_projection);
-		}
+		if ( u <= 30 )
+			gyri[0].item(i) = 1;
 		
-		
-// 		map_global["lon"].insert(130);
-		
-		float u=0;
-		float v=0;
-		
-		//float step=0;
-		
-		//int lon[]={0,16,43,58,100,287,303,340,360};
-		//int lat[]={2,30,79,98,125,179};
-		
-		std::set<float>::const_iterator it_lon, it_lat, it_lon2, it_lat2;
-		
-/*		it_lat=map_global["lat"].begin();
+		/*
 		for(; it_lat!=map_global["lat"].end(); ++it_lat)
 		{
 			it_lat2=it_lat;
 			it_lat2++;
-			if( it_lat2!=map_global["lat"].end() )
-			{
-				std::cout<<"latitude"<<std::endl;
-				std::cout<<(*it_lat)<<" - "<<(*it_lat2)<<std::endl;
-			
-				it_lon=map_global["lon"].begin();
-				for(; it_lon!=map_global["lon"].end(); ++it_lon)
-				{
-					it_lon2=it_lon;
-					it_lon2++;
-					if( it_lon2!=map_global["lon"].end() )
-						std::cout<<(*it_lon)<<" - "<<(*it_lon2)<<std::endl;
-				}
-			}
-		}*/
-		
-		
-		for(int i=0;i<size;i++)
-		{
-			gyri[0].item(i)=0;
-			int cpt=2;
-			
-			u=latitude[0].item(i);
-			v=longitude[0].item(i);
-			
-			it_lat=map_global["lat"].begin();
-			for(; it_lat!=map_global["lat"].end(); ++it_lat)
-			{
-				it_lat2=it_lat;
-				it_lat2++;
-				if( it_lat2!=map_global["lat"].end() )
-				{
-// 					std::cout<<"latitude"<<std::endl;
-// 					std::cout<<(*it_lat)<<" - "<<(*it_lat2)<<std::endl;
-			
-					it_lon=map_global["lon"].begin();
-					for(; it_lon!=map_global["lon"].end(); ++it_lon)
-					{
-						it_lon2=it_lon;
-						it_lon2++;
-						if( it_lon2!=map_global["lon"].end() )
-						{
-// 							std::cout<<(*it_lon)<<" - "<<(*it_lon2)<<std::endl;
-							if( u>(*it_lat) && u<=(*it_lat2) && v>(*it_lon) && v<=(*it_lon2) )
-							{
-								gyri[0].item(i)=cpt;
-// 								std::cout<<(*it_lon)<<" - "<<(*it_lon2)<<std::endl;
-// 								std::cout<<"CPT= "<<cpt<<std::endl;
-							}
-							cpt++;
-						}
-					}
-				}
-			}
-			
-			//Pole cingulaire
-			
-			if(u<=30)
-				gyri[0].item(i)=1;
-			
-			/*
-			for(; it_lat!=map_global["lat"].end(); ++it_lat)
-			{
-				it_lat2=it_lat;
-				it_lat2++;
-				it_lon=map_global["lon"].begin();
+			it_lon=map_global["lon"].begin();
 // 				std::cout<<(*it_lat)<<" - "<<(*it_lat2)<<std::endl;
 				if( it_lat2!=map_global["lat"].end() )
 				{
@@ -380,8 +321,6 @@ using namespace std;
 							if( u>(*it_lat) && u<=(*it_lat2) && v>(*it_lon) && v<=(*it_lon2) )
 							{
 								gyri[0].item(i)=cpt;
-// 								std::cout<<(*it_lon)<<" - "<<(*it_lon2)<<std::endl;
-// 								std::cout<<"CPT= "<<cpt<<std::endl;
 								cpt++;
 							}
 						}
@@ -389,7 +328,7 @@ using namespace std;
 				}
 			}*/
 			
-/*			//Pre-central
+			/*//Pre-central
 			if( u<=16 && v>2 && v<= 30)
 				gyri[0].item(i)=1;
 				
@@ -519,14 +458,10 @@ using namespace std;
 				
 			if( u>340 && u<=360 && v>125 && v<= 179)
 				gyri[0].item(i)=40;*/
-		}
-		
-// 		std::cout<<"compteur="<<cpt/size<<std::endl;
-		
-		Writer<Texture1d> gW(adOut);
-		gW.write(gyri);
-		
-		
 	}
+		
+	Writer<Texture1d> gW(adOut);
+	gW.write(gyri);
+}
 
 
