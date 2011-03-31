f9349d728d20666ccf7fc8f2cfd582b9a2d91a84
coulon <coulon@bioproj.invalid>
2011-03-31 16:35:08 +0000

    This program now also compute the sulcal profile (cf EMBC2011 paper)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-private/trunk@41849 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/commands/AimsSulcusNormalizeDepthProfile/main.cc
+++ src/commands/AimsSulcusNormalizeDepthProfile/main.cc
@@ -27,9 +27,9 @@ uint closer(AimsSurfaceTriangle surface, Point3df p);
 
 int main( int argc, const char** argv )
 {
-     string fileOut, fileMesh, fileX, fileY;
+     string fileOut, fileMesh, fileX, fileY, fileC;
 
-     AimsApplication    app( argc, argv, "Compute normalized depth profile from " );
+     AimsApplication    app( argc, argv, "Compute depth and profile curve from SC " );
      try
      {
           app.addOption( fileMesh, "-m", "input mesh" );
@@ -38,9 +38,11 @@ int main( int argc, const char** argv )
           app.alias( "--xcoord", "-x" );
           app.addOption( fileY, "-y", "y coordinate texture" );
           app.alias( "--ycoord", "-y" );
+          app.addOption( fileC, "-d", "dist to plan file" );
+          app.alias( "--dist", "-d");
           app.addOption( fileOut, "-o", "output text file" );
           app.alias( "--out", "-o" );
-          
+
           app.initialize();
           
           cout << "reading triangulation   : " << flush;
@@ -56,6 +58,11 @@ int main( int argc, const char** argv )
           Reader<TimeTexture<float> > texXR( fileX );
           TimeTexture<float> texX;
           texXR >> texX ;
+          Reader<TimeTexture<float> > texCR( fileC );
+          TimeTexture<float> texC;
+          texCR >> texC ;
+
+
           cout << "done " << endl;
           
           // GENERER LES SEGMENTS ICI ET CALCULER LES PROFONDEURS
@@ -64,6 +71,7 @@ int main( int argc, const char** argv )
           std::list<short>::iterator coordIt;
 /*          std::map<uint, AimsSegments> isoseg;*/
           std::map<uint, float> depth;
+          std::map<uint, float> curvM, curvM2, sig, smooth;
           IsoLine par(surface, texY);
           std::cout << "Processing values" << std::endl;
           uint i;
@@ -77,6 +85,11 @@ int main( int argc, const char** argv )
                std::vector<Point3df>  vert=parallele.vertex();
                std::vector<AimsVector<uint,2> > poly=parallele.polygon();
                std::vector<AimsVector<uint,2> >::iterator polyIt=poly.begin();
+               float curv=0.0, c;
+               float curv2=0.0, c2;
+
+               int ncurv2=0;
+               int ncurv=0;
                for ( ; polyIt!=poly.end(); ++polyIt)
                {
                     uint v1=(*polyIt)[0];
@@ -87,31 +100,101 @@ int main( int argc, const char** argv )
                     uint i2=closer(surface, p2);
                     float x1=texX.item(i1), x2=texX.item(i2);
                     float d=norm(p1-p2);
+                    c=texC.item(i1);
                     if (x1<100.0)
                     {
                          if (x2<=100.0) d1+=d;
                          else {d1+=d; d2+=d;}
+                         if ((x1>=0) && (x1<=100))
+                         {
+                        	 curv2+=c;
+                             ncurv2++;
+                         }
                     }
                     else
                     {
                          if (x2>=100.0) d2+=d;
                          else {d1+=d; d2+=d;}
+                         if ((x1>=100) && (x1<=200))
+                         {
+                             curv+=c;
+                             ncurv++;
+                         }
                     }
                }
-               std::cout << d1 << "\t" << d2 << std::endl;
+               if (ncurv!=0)
+            	   curv=curv/float(ncurv);
+               else curv=0.0;
+               if (ncurv2!=0)
+                   curv2=curv2/float(ncurv2);
+               else curv2=0.0;
+               //std::cout << d1 << "\t" << d2 << std::endl;
                if (d1<=d2) depth.insert(std::pair<uint, float>(i, d1));
                else depth.insert(std::pair<uint, float>(i, d2));
+               curvM.insert(std::pair<uint, float>(i, curv));
+               curvM2.insert(std::pair<uint, float>(i, curv2));
           }
 
           cout << "done" << endl;
           
           ofstream fout(fileOut.c_str());
+          float minC=100.0;
+          uint imin=0, imax;
           for (i=0; i<=100; i++)
           {
-               fout << i << "\t" << depth[i] << std::endl;
+              fout << i << "\t" << depth[i] << "\t" << curvM[i] << "\t" << curvM2[i] << std::endl;
           }
           fout.close();
 
+          // smoothing before looking for extrema
+
+          cout << "Smoothing profile before detecting landmarks" << endl;
+       	  for (i=0; i<=100; i++)
+       	  {
+       		  smooth.insert(std::pair<uint, float>(i, curvM[i]));
+       		  sig.insert(std::pair<uint, float>(i, curvM[i]));
+       	  }
+
+       	  float lapl;
+          for (uint t=0; t<24; t++)
+           {
+        	  for (i=0; i<=100; i++)
+        		  sig[i]=smooth[i];
+
+         	  for (i=0; i<=100; i++)
+         	  {
+         		  if (i==0)
+         			  lapl=sig[1]-sig[0];
+         		  else if (i==100)
+         			  lapl=sig[99]-sig[100];
+         		  else
+         			  lapl=sig[i-1]-2*sig[i]+sig[i+1];
+         		  smooth[i]=sig[i]+0.25*lapl*0.5;
+         	  }
+           }
+
+          cout << "looking for extrema" << endl;
+
+          for (i=0; i<=66; i++)
+          {
+        	  if (smooth[i] < minC)
+        	  {
+        		  imin=i;
+        		  minC=smooth[i];
+        	  }
+          }
+
+          imax=imin;
+          for (i=imin+1; i<100; i++)
+          {
+        	  if ((smooth[i] > smooth[i+1]) && (smooth[i] > smooth[i-1]))
+        	  {
+        		  imax=i;
+        		  break;
+        	  }
+          }
+          cout << "Elbow (min dist point):                 " << imin << endl;
+          cout << "Wrist (1st local max dist point after elbow): " << imax << endl;
           return EXIT_SUCCESS;
      }
      catch( user_interruption & )
