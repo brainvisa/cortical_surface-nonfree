56fdcb41bdb1b3d8f45fe4708674cb84bf819639
le-troter <le-troter@bioproj.invalid>
2011-05-06 15:17:00 +0000

    clean map of probability (sulcal lines)
    git-svn-id: https://bioproj.extra.cea.fr/neurosvn/brainvisa/cortical_surface/cortical_surface-private/trunk@42701 613412ce-1361-4b7a-93bf-a3c75af1bae4

--- src/library/surfacereferential/sulcalLinesGeodesic.h
+++ src/library/surfacereferential/sulcalLinesGeodesic.h
@@ -40,30 +40,17 @@ class SulcalLinesGeodesic
 
     //Parametres pour l'execution
     string _adrMesh;
-
     string _adrRootsLat;
     string _adrRootsLon;
-
     string _adrCurv;
     string _adrGeodesicDepth;
-    //string _adrBassinsDepthNorm;
 
     int _strain;
     int _extremeties_method;
-    int _segmentation_method;
+    int _constraint_type;
 
     AimsSurfaceTriangle _mesh;
-
-//    string _adrBassins;
-//       string _adrLines;
-//
-//       string _adrLatGeodesicOut;
-//       string _adrLonGeodesicOut;
-//
-//
     std::vector<std::set<uint> > _neigh;
-//
-//    TimeTexture<short> _texProbaPath;
 
     TimeTexture<short> _rootsLon;
     TimeTexture<short> _rootsLat;
@@ -71,36 +58,9 @@ class SulcalLinesGeodesic
     TimeTexture<float> _geoDepth;
     TimeTexture<float> _geoDepthNorm;
 
-
-
-//    TimeTexture<float> _texCurvSeuil;
-//
-//    TimeTexture<short> _texBassins;
-//    TimeTexture<float> _texBassinsDepthNorm;
-//
-//    TimeTexture<short> _texBassinsLat;
-//    TimeTexture<short> _texBassinsLon;
-
-//      geodesic::Mesh _meshSPc;
-//      geodesic::Mesh _meshSP;
-//
-//      geodesic::GeodesicAlgorithmDijkstra *dijkstra_algorithm;
-//
-//      vector<double> _pointsSP;
-//      vector<unsigned> _facesSP;
-
-//    set<int> _listIndexVertexFill;
-//    map<int,set<int> > _mapBassins;
-//
-//    set<int> _listIndexLon;
-//    set<int> _listIndexLat;
-//
-//    map<int, set<int> > _mapConstraintLat;
-//    map<int, set<int> > _mapConstraintLon;
-
     //Constructor
     SulcalLinesGeodesic( string & adrMesh,string & adrCurv, string & adrGeodesicDepth,
-        string & adrRootsLon, string & adrRootsLat, int extremeties_method, int segmentation_method, int strain );
+        string & adrRootsLon, string & adrRootsLat, int extremeties_method, int constraint_type, int strain );
 
     ~SulcalLinesGeodesic();
 
@@ -130,6 +90,9 @@ class SulcalLinesGeodesic
     void interRootsDilBasins(TimeTexture<short> &texBasins,TimeTexture<short> &texDil,TimeTexture<short> &texInter);
     void cleanBasins(map<int,set<int> > &mapBasins,TimeTexture<short> &texBasins,int nbPoint);
     void contourBasins(map<int,set<int> > &mapBasins,TimeTexture<short> &texBasins,map<int,set<int> > &mapContourBasins,TimeTexture<short> &texContourBasins);
+    void computeProbabiltyMap(map<int,set<int> > &mapContourBasins,TimeTexture<short> &texContourBasins,TimeTexture<float> &texProba);
+    void normalizeProbabiltyMap(map<int,set<int> > &mapBasins, map<int,set<int> > &mapContourBasins,TimeTexture<short> &texContourBasins,TimeTexture<float> &texProba,TimeTexture<float> &texProbaNorm);
+    void textureBin2Label(TimeTexture<short> &texLabel, TimeTexture<short> &texIn, TimeTexture<short> &texOut);
 
     void sulcalLinesExtract_probability(map<int,set<int> > &mapBasins, TimeTexture<short> &texBasins);
 
